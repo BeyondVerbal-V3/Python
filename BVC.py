a595645bab01fb315d570ef6c9098540f472a7c7
Ariel Lapinin <ariel.lapinin@beyondverbal.com>
2016-11-28 15:41:10 +0200

    Update BVC.py

--- BVC.py
+++ BVC.py
@@ -6,13 +6,11 @@ def getAnalysis(API_Key,WavPath):
     token = res.json()['access_token']
     headers={"Authorization":"Bearer "+token}
     pp = requests.post("https://apiv3.beyondverbal.com/v3/recording/start",json={"dataFormat": { "type":"WAV" }},verify=False,headers=headers)
-    print(pp.status_code)
     recordingId = pp.json()['recordingId']
     with open(WavPath,'rb') as wavdata:
         r = requests.post("https://apiv3.beyondverbal.com/v3/recording/"+recordingId,data=wavdata, verify=False, headers=headers)
-        print(r.status_code)
-        print(r.json())
         return r.json()
 
 
-getAnalysis("API Key","C:\\YouFilePath.wav")
+json = getAnalysis("API Key","C:\\YouFilePath.wav")
+print(json)
